8032fbbe7bc41cc041824365fd6897dddd8b7c16
Freddie Rosario <frosario777@gmail.com>
2015-09-19 12:57:28 -0400

    Focus on the movieProvider methods I actually need.

--- app/src/main/java/com/example/frosario/popularmovies/movieProvider.java
+++ app/src/main/java/com/example/frosario/popularmovies/movieProvider.java
@@ -22,46 +22,12 @@ public class MovieProvider extends ContentProvider {
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        //TODO: Make more efficient by keeping posters we can reuse
-        int postersDeleted = 0;
-        Cursor cursor = db.rawQuery("select poster_path from movies",null);
-
-        //Delete movie posters from filesystem
-        if (cursor != null) {
-            if (cursor.moveToFirst()) {
-                do {
-                    String poster_path = cursor.getString(cursor.getColumnIndex("poster_path"));
-                    File image_file = new File(poster_path.substring(1));
-                    if (image_file.exists()){
-                        if (image_file.delete()) {
-                            postersDeleted += 1;
-                        } else {
-                            Log.e(TAG,"Error deleting image poster");
-                        }
-                    }
-                }while (cursor.moveToNext());
-            }
-        }
-
-        //Empty table after all posters are removed
-        db.delete("movies",null,null);
-
-        return postersDeleted;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public String getType(Uri uri) {
-        UriMatcher uriMatcher = buildUriMatcher();
-        int match = uriMatcher.match(uri);
-
-        switch (match) {
-            case 1:
-                return "application/json";
-            case 2:
-                return "application/json";
-            default:
-                throw new UnsupportedOperationException("Not yet implemented");
-        }
+        throw new UnsupportedOperationException();
     }
 
     @Override
@@ -114,15 +80,13 @@ public class MovieProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
-        Cursor cursor = db.query("movies", projection, selection, selectionArgs, null, null, sortOrder);
-        return cursor;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
-        int rowsUpdated = db.update("movies", values, selection, selectionArgs);
-        return rowsUpdated;
+        throw new UnsupportedOperationException();
     }
 
     private UriMatcher buildUriMatcher() {
