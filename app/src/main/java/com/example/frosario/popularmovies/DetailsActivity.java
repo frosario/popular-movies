6bdc26b1a805d02116971663a3ba12b510aa9f17
Freddie Rosario <frosario777@gmail.com>
2015-10-04 16:52:47 -0400

    Removed unused options menu code in DetailsActivity

--- app/src/main/java/com/example/frosario/popularmovies/DetailsActivity.java
+++ app/src/main/java/com/example/frosario/popularmovies/DetailsActivity.java
@@ -23,25 +23,6 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_details, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @Override
     protected void onResume() {
         super.onResume();
